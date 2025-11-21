5957e2f8f3bec1175007df831866ae781570292b
Manas-Siddharth <kopurimanassiddharth@gmail.com>
2025-11-21 19:06:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
+
 const app = express();
 app.use(express.json());
 
-const VERIFY_TOKEN = "mysecretkey";   // SAME TOKEN IN META
+const VERIFY_TOKEN = "mysecretkey";
 
-// WEBHOOK VERIFICATION (Meta calls this first)
+// WEBHOOK VERIFICATION
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
   if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("WEBHOOK VERIFIED!");
     return res.status(200).send(challenge);
   } else {
     return res.sendStatus(403);
@@ -24,6 +24,5 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-// IMPORTANT: Vercel uses PORT provided by environment.
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on " + PORT));
+// EXPORT FOR VERCEL
+export default app;
